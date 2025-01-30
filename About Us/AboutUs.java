141d567ad63e025f576782a0d1ef817315095e45
nnzvha <168803035+nnzvha@users.noreply.github.com>
2025-01-30 18:02:50 +0800

    Update AboutUs.java

--- About Us/AboutUs.java
+++ About Us/AboutUs.java
@@ -1,15 +1,19 @@
 package com.example.masjidfinder;
+
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 public class AboutUs extends AppCompatActivity {
- @Override
- protected void onCreate(Bundle savedInstanceState) {
- super.onCreate(savedInstanceState);
- EdgeToEdge.enable(this);
- setContentView(R.layout.activity_about_us);
- }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about_us);
+    }
 }
